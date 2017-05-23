665a05354ac64e1c59fc1a64a3e7254b7ec25364
Adam Grzegorzewski <adamg@cakesolutions.net>
2017-05-23 16:41:20 +0100

    Jenkinsfile update

--- test-python.sh
+++ test-python.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-docker run -i -t themcmurder/ubuntu-python-pip /bin/bash
-pip -q install selenium requests behave promise git
-git clone -q https://github.com/hugeinc/behave-parallel
-cd behave-parallel
+./docker run -i -t themcmurder/ubuntu-python-pip /bin/bash
+./pip -q install selenium requests behave promise git
+./git clone -q https://github.com/hugeinc/behave-parallel
+./cd behave-parallel
 ./setup.py --quiet install
-cd ..
+./cd ..
 scennumber=$(sed 's/Scenario:/Scenario:\'$'\n/g' features/*.feature | grep -c "Scenario:")
 
 #Running the test suite; results are created in TestResults directory
 ./behave-parallel/bin/behave-parallel --processes $scennumber --junit --junit-directory TestResults
 
 #Executing the brute force attack
-docker cp bruteforce.py themcmurder/ubuntu-python-pip:/bruteforce.py
-docker cp logins.txt themcmurder/ubuntu-python-pip:/logins.txt
-docker cp passwords.txt themcmurder/ubuntu-python-pip:/passwords.txt
+./docker cp bruteforce.py themcmurder/ubuntu-python-pip:/bruteforce.py
+./docker cp logins.txt themcmurder/ubuntu-python-pip:/logins.txt
+./docker cp passwords.txt themcmurder/ubuntu-python-pip:/passwords.txt
 ./bruteforce.py $TARGET_URL $LOGINS $PASSWORDS
