8785ffe4aa87bbe45331431fbb7fe9ab1607f9d5
Adam Grzegorzewski <adamg@cakesolutions.net>
2017-05-24 15:27:36 +0100

    Jenkinsfile update

--- sed.sh
+++ sed.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 scennumber=$(sed 's/Scenario:/Scenario:\'$'\n/g' features/*.feature | grep -c "Scenario:")
+python behave-parallel/bin/behave-parallel --processes $scennumber --junit --junit-directory TestResults
