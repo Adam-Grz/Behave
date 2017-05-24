a977601f89dbb8953a3a0822db99b8be6ff356d5
Adam Grzegorzewski <adamg@cakesolutions.net>
2017-05-24 15:15:47 +0100

    Jenkinsfile update

--- sed.sh
+++ sed.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-scennumber=$(sed 's/Scenario:/Scenario:'$\'\n/g' features/*.feature | grep -c "Scenario:")
+scennumber=$(sed 's/Scenario:/Scenario:\'$'\n/g' features/*.feature | grep -c "Scenario:")
