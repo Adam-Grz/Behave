49b8ee448bb64f0c804a8375aae71df25d74f6c4
Adam Grzegorzewski <adamg@cakesolutions.net>
2017-05-31 14:54:50 +0100

    Updated Python tests with xvfb

--- features/environment.py
+++ features/environment.py
@@ -1,15 +1,17 @@
 from behave import *
 from selenium import webdriver
 from promise import Promise
+from xvfbwrapper import Xvfb
 
 def before_scenario(context, scenario):
 	if 'browser' in scenario.tags:
+		context.xvfb = Xvfb(width=1280, height=720)
+		context.xvfb.start()
 		context.driver = webdriver.Firefox()
-#		context.driver.set_window_size(1500, 1000)
-#		context.driver.maximize_window()
 		context.driver.get('https://cakesolutions.github.io/cake-qa-test/#/')
 		return context.driver
 
 def after_scenario(context, scenario):
 	if 'browser' in scenario.tags:
+		context.xvfb.stop()
 		context.driver.quit()
