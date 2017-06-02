019b07864e37966eb41b06277eea0df6acfdf2a9
Adam Grzegorzewski <adamg@cakesolutions.net>
2017-06-02 08:11:51 +0100

    Added JS tests with Xvfb

--- JavaScript/run.js
+++ JavaScript/run.js
@@ -1,15 +1,46 @@
 var fs = require('fs');
 var files = fs.readdirSync('./test/specs');
+var SpecReporter = require('/usr/lib/node_modules/jasmine-spec-reporter').SpecReporter;
+var DisplayProcessor = require('/usr/lib/node_modules/jasmine-spec-reporter').DisplayProcessor;
+
+function TimeProcessor(configuration) {
+}
+function getTime() {
+    var now = new Date();
+    return now.getHours() + ':' +
+           now.getMinutes() + ':' +
+           now.getSeconds()
+}
+TimeProcessor.prototype = new DisplayProcessor();
+TimeProcessor.prototype.displaySuite = function (suite, log) {
+  return getTime() + ' - ' + log;
+};
+TimeProcessor.prototype.displaySuccessfulSpec = function (spec, log) {
+  return getTime() + ' - ' + log;
+};
+TimeProcessor.prototype.displayFailedSpec = function (spec, log) {
+  return getTime() + ' - ' + log;
+};
+TimeProcessor.prototype.displayPendingSpec = function (spec, log) {
+  return getTime() + ' - ' + log;
+};
+
+var reporter = new SpecReporter({
+    customProcessors: [TimeProcessor]
+});
 
 exports.config = {
-  framework: 'jasmine',
+  framework: 'jasmine2',
+    onPrepare: function () {
+  jasmine.getEnv().addReporter(reporter);
+  },
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['./test/specs/test_*.js'],
   jasmineNodeOpts: {
     showColors: true,
   },
   multiCapabilities: [{
-        browserName: 'chrome',
+        browserName: 'firefox',
         shardTestFiles: true,
         maxInstances: files.length,
     }]
